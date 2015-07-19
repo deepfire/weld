c985d19a581a16ee3feff1f7524f8565d83dd077
Kosyrev Serge <_deepfire@feelingofgreen.ru>
2015-07-19 23:58:51 +0300

    author-id.sh:  parametrize the file correlating nicknames to author names/emails

--- author-id.sh
+++ author-id.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 
 username="$1"
+authors_db="${2:-AUTHORS}"
 
-line="$(grep -w ^${username} AUTHORS)"
+line="$(grep -w ^${username} ${authors_db})"
 grep_exit_code="$?"
 test "${grep_exit_code}" != "0" && {
 	echo "ERROR: unknown author username: ${username}" >&2
